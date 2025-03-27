0a67216a614c62a15e5c49cb64793612cfba51fe
nineeko <gkxm1013@naver.com>
2025-03-27 12:44:44 +0900

    25.03.27 pm 12:44

---
+++ chap01-persistence-context-lecture-source/src/test/java/com/neeko/section02/crud/EntityManagerCRUDTest.java
@@ -0,0 +1,54 @@
+package com.neeko.section02.crud;
+
+import com.neeko.section01.entitymanager.EntityManagerGenerator;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+class EntityManagerCRUDTest {
+private EntityManagerCRUD entityManagerCRUD;
+
+    @BeforeEach
+    void init(){
+        this.entityManagerCRUD = new EntityManagerCRUD();
+
+    }
+    @DisplayName("메뉴 코드로 메뉴 조회")
+    @ParameterizedTest
+    @CsvSource({"1,1","2,2","3,3"})
+    void testFindMenuByMenuCode(int menuCode, int expected) {
+        // when
+        Menu foundMenu = entityManagerCRUD.findMenuByMenuCode(menuCode);
+        // then
+        assertEquals(expected, foundMenu.getMenuCode());
+        System.out.println("foundMenu = " + foundMenu);
+    }
+
+    private static Stream<Arguments> newMenu(){
+        return Stream.of(
+                Arguments.of(
+                        "신메뉴", 3500, 4, "Y"
+                )
+        );
+    }
+
+    @DisplayName("새로운 메뉴 추가")
+    @ParameterizedTest
+    @MethodSource("newMenu")
+    void testRegist(String menuName, int menuPrice, int categoryCode,String orderableStatus) {
+        // when
+        Menu menu = new Menu(menuName,menuPrice,categoryCode,orderableStatus);
+        Long count = entityManagerCRUD.saveAndReturnAllCount(menu);
+
+        // then
+        assertEquals(26, count);
+    }
+
+}
